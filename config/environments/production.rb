5c45b3d8c25b4157e5eb3c15e5d48061ec31329d
Ivan Vergés <ivan@pokecode.net>
2022-11-09 12:14:06 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.initialize_on_precompile = false
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
