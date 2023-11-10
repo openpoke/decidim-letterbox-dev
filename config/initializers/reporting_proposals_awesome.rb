e69e4b24ce55e1178849b9374d9fce4ca9857ecd
Ivan Vergés <ivan@pokecode.net>
2023-11-10 13:21:42 +0100

    hack reporting proposals

--- config/initializers/reporting_proposals_awesome.rb
+++ config/initializers/reporting_proposals_awesome.rb
@@ -1,25 +1,26 @@
 
-  Decidim.component_registry.find(:reporting_proposals).tap do |component|
-    component.settings(:global) do |settings|
-      settings.attribute :awesome_voting_manifest,
-                         type: :select,
-                         default: "",
-                         choices: -> { ["default"] + Decidim::DecidimAwesome.voting_registry.manifests.map(&:name) },
-                         readonly: lambda { |context|
-                           Decidim::Proposals::Proposal.where(component: context[:component]).where.not(proposal_votes_count: 0).any?
-                         }
-      settings.attribute :voting_cards_box_title,
-                         type: :string,
-                         translated: true
-      settings.attribute :voting_cards_show_modal_help,
-                         type: :boolean,
-                         default: true
-      settings.attribute :voting_cards_show_abstain,
-                         type: :boolean,
-                         default: false
-      settings.attribute :voting_cards_instructions,
-                         type: :text,
-                         translated: true,
-                         editor: true
+Decidim.component_registry.find(:reporting_proposals).tap do |component|
+  component.settings(:global) do |settings|
+    settings.attribute :awesome_voting_manifest,
+                       type: :select,
+                       default: "",
+                       choices: -> { ["default"] + Decidim::DecidimAwesome.voting_registry.manifests.map(&:name) },
+                       readonly: lambda { |context|
+                         Decidim::Proposals::Proposal.where(component: context[:component]).where.not(proposal_votes_count: 0).any?
+                       }
+    settings.attribute :voting_cards_box_title,
+                       type: :string,
+                       translated: true
+    settings.attribute :voting_cards_show_modal_help,
+                       type: :boolean,
+                       default: true
+    settings.attribute :voting_cards_show_abstain,
+                       type: :boolean,
+                       default: false
+    settings.attribute :voting_cards_instructions,
+                       type: :text,
+                       translated: true,
+                       editor: true
 
 	end
+end
